d25d713df7c80dcc5582b2bb054717cd135bc744
Tanmaya <tanmayanaik2020@gmail.com>
2026-02-17 00:29:44 +0530

    Added the code of Nested Router

--- REACT FROM SCRACT/React_Router/REact_router/src/Components/About.jsx
+++ REACT FROM SCRACT/React_Router/REact_router/src/Components/About.jsx
@@ -6,7 +6,7 @@ export default function About(){
     return (
         <>
         <h1>hello bro welcome to About page</h1>
-        <Outlet></Outlet>
+         <Outlet></Outlet>  {/* OUTLET HELPS US TO KNOW THAT WHICH IS GOING TO DISPLAY OUT OF OTHER COMPONENT PRESENT IN THE NESTED ROUTER */}
         </>
     )
 
