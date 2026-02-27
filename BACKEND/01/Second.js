74fb93e5f9a0e63931d033b79c6a718e198a6a7c
Tanmaya <tanmayanaik2020@gmail.com>
2026-02-27 16:44:53 +0530

    Added code for backend

---
+++ BACKEND/01/Second.js
@@ -0,0 +1,10 @@
+console.log("Hello Tanu bro i am second")
+
+function sum(a,b){
+    console.log(a+b);
+}
+function sub(a,b){
+    console.log(a-b);
+}
+
+module.exports = {sum,sub};
---
+++ BACKEND/01/first.js
@@ -0,0 +1,10 @@
+
+const {sum , sub} = require("./Second");
+
+
+sum(3,5);
+sub(5,3);
+console.log("Hello Tanu bro");
+
+//we need the second.js code in this file to code both simultaneolusly
+//this is called cjs (common js module)
